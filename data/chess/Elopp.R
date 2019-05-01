da987453392b479d897f9dbbaa19736443920e6d
Yuan YAO <37133869+yao-lab@users.noreply.github.com>
2019-05-01 11:48:03 +0800

    Add files via upload

---
+++ data/chess/Elopp.R
@@ -0,0 +1,74 @@
+# Elo++ by Quanwu Xiao, Microsoft, Beijing
+# 
+# Used for Yuan Yao's course project in Peking University, 2012
+#
+# Reference: 
+#	How I won the "Chess Ratings -- Elo vs the Rest of the World" Competition
+#	Yannis Syismanis, arXiv:1012.4571v1
+#
+# The code is not optimized, which takes about 10 mins and returns the mean square root error 
+# 0.68896
+
+# system.time(source("Elopp.R", echo=TRUE))
+
+training <- as.matrix(read.csv("data/training_data.csv"))
+
+
+agg1 <- aggregate(x=training[,4],by=list(training[,2]),FUN="sum")
+agg2 <- aggregate(x=training[,4],by=list(training[,3]),FUN="sum")
+
+maxplayer <- max(agg1[,1],agg2[,1])
+ri <- rep(0,maxplayer)
+
+ngame <- nrow(training)
+pred <- rep(0,ngame)
+
+w <- rep(0,ngame)
+w <- (training[,1]/max(training[,1]))^2
+
+train <- as.matrix(training)
+games <- rbind(train[,2:3], train[,3:2])
+nn <- split(games[,1], games[,2])
+nnum <- aggregate(games[,1],by=list(games[,2]),FUN="length")
+nnn <- rep(0, maxplayer)
+nnn[nnum[,1]] <- nnum[,2]
+
+wn <- split(c(w,w), games[,2])
+
+lamd=0.77
+gama=0.2
+
+P <- 50
+for (p in 1:P)
+{
+	
+	ai <- rep(0,maxplayer)
+	for (i in 1:maxplayer)
+	{
+		k <-  nn[[toString(i)]]
+		wk <- wn[[toString(i)]]
+		ai[i] <- sum(wk*ri[k])/sum(wk)
+	}
+
+	yita <- ((1+0.1*P)/(p+0.1*P))^0.602
+	for(i in 1:ngame)
+	{
+		pred[i] <- 1/(1+exp(ri[training[i,3]]-(ri[training[i,2]]+gama)))
+		ri[training[i,2]] <- ri[training[i,2]]-yita*(w[i]*(pred[i]-training[i,4])*pred[i]*(1-pred[i])+lamd/nnn[training[i,2]]*(ri[training[i,2]]-ai[training[i,2]]))
+		ri[training[i,3]] <- ri[training[i,3]]-yita*(-w[i]*(pred[i]-training[i,4])*pred[i]*(1-pred[i])+lamd/nnn[training[i,3]]*(ri[training[i,3]]-ai[training[i,3]]))
+	}
+	# print(head(ri))
+}
+
+
+test <- as.matrix(read.csv("data/test_scores.csv"))
+
+prediction <- 1/(1+exp(ri[test[,3]]-(ri[test[,2]]+gama)))
+
+games_new <- rbind(test[,1:2], test[,c(1,3)])
+actual <- c(test[,4], 1-test[,4])
+prediction <- c(prediction, 1-prediction)
+
+ra <- aggregate(actual, by=list(games_new[,1], games_new[,2]), FUN=sum)$x
+rp <- aggregate(prediction, by=list(games_new[,1], games_new[,2]), FUN=sum)$x
+print(sqrt(mean((ra-rp)^2)))
---
+++ data/chess/R/chessHodge.r
@@ -0,0 +1,83 @@
+# system.time(source("chessHodge.r", echo=TRUE))
+# scoring by hodge
+
+rm(list=ls())
+gc()
+library(Matrix)
+
+train <- as.matrix(read.csv("./training_data.csv"))
+valid <- as.matrix(read.csv("./cross_validation_dataset.csv"))
+test <- as.matrix(read.csv("./test_scores.csv"))
+
+players.old <-  sort(unique(c(train[,2], train[,3])))
+nplayer <- length(players.old)
+max.players.old <- players.old[nplayer]
+player.tranform <- rep(0, max.players.old)
+player.tranform[1:max.players.old %in% players.old] <- 1:nplayer
+train[,2] <- player.tranform[train[,2]]
+train[,3] <- player.tranform[train[,3]]
+valid[,2] <- player.tranform[valid[,2]]
+valid[,3] <- player.tranform[valid[,3]]
+test[,2] <- player.tranform[test[,2]]
+test[,3] <- player.tranform[test[,3]]
+
+
+com <- rbind(train[,2], train[,3])
+Y <- train[,4]
+w <- train[,1]/max(train[,1])
+wY <- Y*w
+
+
+nMatch <- nrow(train)
+comPlus1 <- rbind(com, nplayer+1)
+indRow <- rep(1:nMatch, each=3)
+indCol <- as.vector(comPlus1)
+values <- rep(c(1, -1, 1), nMatch) * rep(w, each=3)
+Amat <- sparseMatrix(i=indRow, j=indCol, x=values)
+
+
+comDouble <- t(cbind(com, com[c(2,1),]))
+neighborList <- split(x=comDouble[,1], f=comDouble[,2])
+weightList <- split(x=rep(w,2), f=comDouble[,2])
+sumNeighborWeight <- lapply(weightList, FUN=sum)
+getLijInd <- function(i) {
+	v1 <- weightList[[i]]/sumNeighborWeight[[i]]
+	v2 <- sapply(split(x=v1, f=neighborList[[i]]), FUN=sum)
+	jInd <- as.integer(names(v2))
+	return(jInd)
+}
+getLijValue <- function(i) {
+	v1 <- weightList[[i]]/sumNeighborWeight[[i]]
+	v2 <- sapply(split(x=v1, f=neighborList[[i]]), FUN=sum)
+	names(v2) <- NULL
+	LValue <- -v2
+	return(LValue)
+}
+indColList <- lapply(1:nplayer, FUN=getLijInd)
+indCol <- unlist(indColList)
+indRow <- rep(as.integer(names(neighborList)), times=sapply(indColList, FUN=length))
+values <- unlist(lapply(1:nplayer, FUN=getLijValue))
+indRow <- c(indRow, 1:nplayer)
+indCol <- c(indCol, 1:nplayer)
+values <- c(values, rep(1,nplayer))
+Lmat <- sparseMatrix(i=indRow, j=indCol, x=values, dims=c(nplayer+1, nplayer+1))
+
+getValidError <- function(regParam) {
+#regParam <- 0
+#valid <- test
+Amat <- rBind(Amat, 1)
+wY <- c(wY, 0) #avoid ill matrix 
+r <- solve(as.matrix(crossprod(Amat)) + regParam*crossprod(Lmat), as.vector(crossprod(Amat, wY)))
+
+predY <- r[valid[,2]] - r[valid[,3]] + r[7302]
+predY <- ifelse(predY>1, 1, predY)
+predY <- ifelse(predY<0, 0, predY)
+validY <- valid[,4]
+predScores <- rbind(cbind(valid[,c(1,2)], predY), cbind(valid[,c(1,3)], 1-predY))
+aggpredScore <- aggregate(predScores[,3], by=list(predScores[,1], predScores[,2]), FUN=sum)
+validScores <- rbind(cbind(valid[,c(1,2)], validY), cbind(valid[,c(1,3)], 1-validY))
+aggvalidScore <- aggregate(validScores[,3], by=list(validScores[,1], validScores[,2]), FUN=sum)
+err <- sqrt(crossprod(aggpredScore[,3] - aggvalidScore[,3])/nrow(aggpredScore))
+return(err)
+}
+sapply(exp(-10:-8), FUN=getValidError)
